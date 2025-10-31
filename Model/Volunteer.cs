f7d322e8be3024498951aeb57233db4b53919df6
Mathabo Motaung <moatung20mathabo>
2025-10-31 14:16:56 +0200

    Added the new ApprovedDate column to the Volunteers database table.

--- Model/Volunteer.cs
+++ Model/Volunteer.cs
@@ -3,10 +3,10 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GiftOfTheGiver_Foundation.Models
 {
-    [Table("Volunteers")] // This explicitly names your database table
+    [Table("Volunteers")]
     public class Volunteer
     {
-        [Key] // Marks this as the Primary Key
+        [Key]
         public int VolunteerId { get; set; }
 
         [Required]
@@ -17,7 +17,7 @@ namespace GiftOfTheGiver_Foundation.Models
         [Required]
         [StringLength(100)]
         [Display(Name = "How would you like to help?")]
-        public string HelpType { get; set; } // Based on id="helpType" [cite: 3]
+        public string HelpType { get; set; }
 
         [Required]
         [StringLength(50)]
@@ -26,6 +26,7 @@ namespace GiftOfTheGiver_Foundation.Models
 
         [Required]
         [StringLength(50)]
-        public string Availability { get; set; } // Based on id="availability" [cite: 6]
+        public string Availability { get; set; }
+        public DateTime? ApprovedDate { get; set; } // Nullable, as it's 'Pending' at first
     }
 }
